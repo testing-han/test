006e94f1777424a20a34c67c99923270f8fd037d
aharsha27 <aharsha.vardhanreddy-ext@pernod-ricard.com>
2026-02-06 15:08:24 +0530

    Update app.sh

--- app.sh
+++ app.sh
@@ -1,1 +1,9 @@
+#!bin/bash
+
 echo "Hello World"
+echo "Build Date: $(date)" > pub/info
+echo "Repo Name:" $GITHUB_REPOSITORY >> pub/info
+echo "Build number:" $GITHUB_RUN_NUMBER >> pub/info
+echo "Git Commit:" $GITHUB_SHA >> pub/info
+echo "Git Branch:" $GITHUB_REF_NAME >> pub/info
+echo "Git Tag:" $GITHUB_REF_NAME >> pub/info
